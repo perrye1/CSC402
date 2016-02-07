ed303f717ac2ea6ce61ccc9941f53e8aa1d91992
Ethan Perry <perrye1@nku.edu>
2016-02-06 21:36:23 -0500

    finished

--- csc402prog1/src/project1.java
+++ csc402prog1/src/project1.java
@@ -12,7 +12,7 @@ public class project1{
 	
 	public static void main(String args[]){
 		
-			final int SIZE = 100000;
+			final int SIZE = 1000;
 			int[] array1 = new int[SIZE] ;
 			ArrayList<Integer> array2 = new ArrayList<Integer>();
 			Random rand = new Random();
@@ -33,7 +33,7 @@ public class project1{
 			long duration1 = (endTime1 - startTime1)/1000000;
 			long duration2 = (endTime2 - startTime2)/1000000;
 			
-			System.out.println("Sorting the native array took " + duration1 + "ms, and sorting the fixed array took " + duration2 + "ms.");
+			System.out.println("Java: Sorting the native array took " + duration1 + "ms, and sorting the fixed array took " + duration2 + "ms." + " Size was " + SIZE + ".");
 			
 	}
 	
