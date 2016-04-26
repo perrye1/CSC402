460b200a27ed6e9da45b6533fcf54ce68c61739d
Ethan Perry <perrye1@nku.edu>
2016-04-25 23:29:59 -0400

    finished prog5

--- prog5/Prog5_1/Project5.cpp
+++ prog5/Prog5_1/Project5.cpp
@@ -20,6 +20,7 @@ Purpose:  Project5 Catalog implementation
 
 using namespace std;
 
+// struct defining an item
 struct ItemInfo {
 	string description;
 	double  price;
@@ -32,31 +33,67 @@ struct ItemInfo {
 	}
 };
 
-typedef map<std::string, ItemInfo> Catalog;
-typedef map<std::string, int> Order;
+// typedefs for the Order and Catalog
+typedef map<string, ItemInfo> Catalog;
+typedef map<string, int> Order;
 
-void readCatalog(Catalog & catalog, const string & fileName) throw(runtime_error); // reads the input file and creates the catalog; throws a runtime_error if the file cannot be opened
-void printCatalog(const Catalog & catalog); // prints the SKU, description, price, and weight of every item in the catalog
-ItemInfo getItemData(const Catalog & catalog, const string & sku); // finds a single item by SKU and returns the details as a struct; returns a dummy struct with the description "Item not found", price 0.00, and weight 0.00 if the SKU is not in the catalog
-void displayOrderItems(const Order & order, const Catalog & catalog); // Lists the SKU, description, and quantity of each type of order item
-void addItem(Order & order, const Catalog & catalog, const string & sku, int quantity) throw (logic_error); // adds item(s) to the order; throws a logic_error if the item cannot be found in the catalog
-void removeItem(Order & order, const string & sku, int quantity) throw (logic_error); // removes items(s) from the order; throws a logic_error if the item cannot be found in the order
-void displayOrderSummary(const Order & order, const Catalog & catalog); // displays the number of unique item types, the total number of items, the total cost, and the total shipping weight
+// forward declarations
+void readCatalog(Catalog & catalog, const string & fileName) throw(runtime_error);
+void printCatalog(const Catalog & catalog);
+ItemInfo getItemData(const Catalog & catalog, const string & sku);
+void displayOrderItems(const Order & order, const Catalog & catalog);
+void addItem(Order & order, const Catalog & catalog, const string & sku, int quantity) throw (logic_error);
+void removeItem(Order & order, const string & sku, int quantity) throw (logic_error);
+void displayOrderSummary(const Order & order, const Catalog & catalog); 
 
 void main(){
-	Catalog catalog;
-	readCatalog(catalog, "Catalog.txt");
-	printCatalog(catalog);
+	Catalog catalog; // create a new catalog object
+
+	// try reading the file into the catalog
+	try{
+		readCatalog(catalog, "Catalog.txt");
+	}
+	catch (runtime_error e){
+		cout << "readCatalog: " << e.what() << endl;
+		cout << endl;
+	}
+
+	printCatalog(catalog); // print out the catalog
+
+	Order order; // create a new order object
+
+	// try adding some items to the order
+	try{
+		addItem(order, catalog, "4123RED", 6);
+		addItem(order, catalog, "576361B", 10);
+		addItem(order, catalog, "93456D", 1);
+	}
+	catch (logic_error e){
+		cout << "addItem: " << e.what() << endl;
+	}
+
+	displayOrderItems(order, catalog); // display all the items in the order
+
+	// try removing some items from the order
+	try{
+		removeItem(order, "576361B", 2);
+		removeItem(order, "93456D", 1);
+	}
+	catch (logic_error e){
+		cout << "removeItem: " << e.what() << endl;
+	}
+
+	displayOrderItems(order, catalog); // display all the items in the order again
+	displayOrderSummary(order, catalog); // display the order summary
 }
 
+// reads the input file and creates the catalog; throws a runtime_error if the file cannot be opened
 void readCatalog(Catalog & catalog, const string & fileName) throw(runtime_error){
 	ifstream infile;
-	try {
-		infile.open(fileName);
+	infile.open(fileName);
+	if (!infile.is_open()){
+		throw runtime_error("File could not be opened");
 	}
-	catch (const runtime_error &e){
-		cerr << " a runtime exception was caught, with message '" << e.what() << "'\n";
-	}	
 	string line = "";
 	while (getline(infile, line)){
 		vector<string> tokens;
@@ -70,6 +107,7 @@ void readCatalog(Catalog & catalog, const string & fileName) throw(runtime_error
 	}
 }
 
+// prints the SKU, description, price, and weight of every item in the catalog
 void printCatalog(const Catalog & catalog){
 
 	cout << setw(10) << left << "SKU";
@@ -87,34 +125,101 @@ void printCatalog(const Catalog & catalog){
 		cout << setw(25) << fixed << setprecision(1) << left << iterator->second.weight;
 		cout << endl;
 	}
+	cout << endl;
 }
 
+// finds a single item by SKU and returns the details as a struct; returns a dummy struct with the description "Item not found", price 0.00, and weight 0.00 if the SKU is not in the catalog
 ItemInfo getItemData(const Catalog & catalog, const string & sku){
 	ItemInfo item;
 	Catalog::const_iterator iterator = catalog.find(sku);
 	if (iterator != catalog.end()){
-		item = ItemInfo();
+		
+		item = ItemInfo(iterator->second.description, iterator->second.price, iterator->second.weight);
 	}
 	else{
-		item = ItemInfo(iterator->second.description, iterator->second.price, iterator->second.weight);
+		item = ItemInfo();
 	}
 	return item;
 }
 
+// Lists the SKU, description, and quantity of each type of order item
 void displayOrderItems(const Order & order, const Catalog & catalog){
+	cout << setw(10) << left << "SKU";
+	cout << setw(55) << left << "Description";
+	cout << setw(12) << left << "Quantity";
+	cout << endl;
+	cout << setfill('=') << setw(80) << "=" << endl;
+	cout << setfill(' ');
 
+	for (Order::const_iterator iterator = order.begin(); iterator != order.end(); iterator++) {
+		ItemInfo item = getItemData(catalog, iterator->first);
+		cout << setw(10) << left << iterator->first;
+		cout << setw(55) << left << item.description;
+		cout << setw(12) << fixed << setprecision(2) << left << iterator->second;
+		cout << endl;
+	}
+	cout << endl;
 }
 
-void addItem(Order & order, const Catalog & catalog, const string & sku, int quantity){
-
+// adds item(s) to the order; throws a logic_error if the item cannot be found in the catalog
+void addItem(Order & order, const Catalog & catalog, const string & sku, int quantity) throw (logic_error){
+	ItemInfo item = getItemData(catalog, sku);
+	if (item.description.compare("Item not found") != 0){
+		Order::iterator iterator = order.find(sku);
+		if (iterator != order.end()){
+			iterator->second += quantity;
+		}
+		else{
+			order.insert(make_pair(sku, quantity));
+		}
+	}
+	else{
+		throw logic_error("Item not found");
+	}
 }
 
+// removes items(s) from the order; throws a logic_error if the item cannot be found in the order
 void removeItem(Order & order, const string & sku, int quantity) throw (logic_error){
+	Order::iterator iterator = order.find(sku);
+	if (iterator != order.end()){
+		if (quantity < iterator->second){
+			iterator->second -= quantity;
+		}
+		else{
+			order.erase(sku);
+		}
+	}
+	else{
+		throw logic_error("Item not found");
+	}
 
 }
 
+// displays the number of unique item types, the total number of items, the total cost, and the total shipping weight
 void displayOrderSummary(const Order & order, const Catalog & catalog){
+	int totalQuantity = 0;
+	int distinctTypes = 0;
+	double totalCost = 0;
+	double totalWeight = 0;
 
+	for (Order::const_iterator iterator = order.begin(); iterator != order.end(); iterator++) {
+		ItemInfo item = getItemData(catalog, iterator->first);
+		totalQuantity += iterator->second;
+		distinctTypes++;
+		totalCost += (item.price * iterator->second);
+		totalWeight += (item.weight * iterator->second);
+	}
+
+	cout << setw(30) << left << "Order Summary";
+	cout << endl;
+	cout << setfill('=') << setw(80) << "=" << endl;
+	cout << setfill(' ');
+
+	cout << setw(30) << left << "Distinct Item Types: " << distinctTypes << endl;
+	cout << setw(30) << left << "Total number of items: " << totalQuantity << endl;
+	cout << setw(30) << fixed << "Total Cost: " << setprecision(2) << totalCost << endl;
+	cout << setw(30) << fixed << "Total Weight: " << setprecision(1) << totalWeight << endl;
+	cout << endl;
 }
 
 
